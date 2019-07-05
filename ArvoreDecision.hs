90050b2fb62ed197ac3cbdebb6651db0187192a1
Gabrielk99 <34044829+Gabrielk99@users.noreply.github.com>
2019-07-05 16:59:49 -0300

    Update ArvoreDecision.hs

--- ArvoreDecision.hs
+++ ArvoreDecision.hs
@@ -6,7 +6,7 @@ module ArvoreDecision where
     --Função para retornar uma classe que mais ocorre numa base de exemplos
     --Entrada: Base, Classes possíveis da descrição
     --Saida: A classe que mais ocorre na base disponível
-    maioria::[Exemplo]->[String]->String 
+    --maioria::[Exemplo]->[String]->String 
     maioria base [] = []
     maioria base classes = maiscomum
                         where 
@@ -16,7 +16,7 @@ module ArvoreDecision where
     
     --Entrada: Base de exemplos, tamanho da base, uma lista de caracteristicas e as classes possíveis
     --Saida: Caracteristica melhor de escolha, onde possuí a maior razão de ganho
-    melhorTeste:: [Exemplo]->Int->[Caracteristicas]->[String]->Caracteristicas
+    --melhorTeste:: [Exemplo]->Int->[Caracteristicas]->[String]->Caracteristicas
     melhorTeste  base tamBase caracteristicas classes = snd(head(sortBy (ordena) igr_list))
                               where 
                               igr_list = iGR base tamBase caracteristicas classes
@@ -24,7 +24,7 @@ module ArvoreDecision where
     --Função para avaliar os valores de uma caracteristica selecionada no processo de construção da arvore para gerar as subarvores
     --Entrada: Caracteristica, uma base, uma lista de caracteristicas atualizada, classes possíveis e a classe mais comum
     --Saida: uma lista de arvores, ou seja subarvores que são filhas da arvore mãe caracteristica
-    avaliaValores::Caracteristicas->[Exemplo]->[Caracteristicas]->[String]->String->[ArvDecision]
+    --avaliaValores::Caracteristicas->[Exemplo]->[Caracteristicas]->[String]->String->[ArvDecision]
     avaliaValores (Nominal (nome,atributo)) base caracteristicas classes maisComum = [Valor (convertToDecisaoNom x) (arvoreDecisao (filtraAtributo nome x base) caracteristicas maisComum classes) | x<-atributo]
     
     avaliaValores (Numerico (nome,atributo)) base caracteristicas classes maisComum = [Valor (convertToDecisaoNum (a,b)) (arvoreDecisao (filtraAtributoNum nome (a,b) base) caracteristicas maisComum classes) | (a,b)<-atributo]                                       
@@ -32,7 +32,7 @@ module ArvoreDecision where
     {- Função principal que ira contruir a arvore -}
     --Entrada: uma base de exemplos, uma lista de caracteristicas, a classe mais comum da base de exemplos e a lista de classes presente na descrição
     --Saida: Uma arvore de decisão
-    arvoreDecisao::[Exemplo]->[Caracteristicas]->String->[String]->ArvDecision                                                   
+    --arvoreDecisao::[Exemplo]->[Caracteristicas]->String->[String]->ArvDecision                                                   
     arvoreDecisao base caracteristicas maisComum classes |null base = Resultado maisComum
                                                          |null caracteristicas = Resultado maisComum 
                                                          |qtdClassMaisComum==(fromIntegral tamBase) = Resultado maisComun
@@ -51,25 +51,25 @@ module ArvoreDecision where
     --Função principal para o calculo de razão de ganho
     --Entrada: Base, tamanho da base, uma lista de caracteristicas e classes possíveis
     --Saida: Uma lista de caracteristica associada a sua razão de ganho
-    iGR :: [Exemplo]->Int->[Caracteristicas]->[String]->[(Double,Caracteristicas)]
+    --iGR :: [Exemplo]->Int->[Caracteristicas]->[String]->[(Double,Caracteristicas)]
     iGR _ _ [] _ = []
     iGR base tamBase (Nominal (nome,atributo):caract) classes |ig /=0 && iv/=0 = (ig/iv,(Nominal (nome,atributo))):iGR base tamBase caract classes
                                                               |otherwise = (0,(Nominal (nome,atributo))):iGR base tamBase caract classes
                                                     where 
-                                                        iv = iV base tamBase nome atributo
+                                                        iv = - (iV base tamBase nome atributo)
                                                         ig = iG base tamBase nome atributo (tail classes)
     
     iGR base tamBase (Numerico (nome,atributo):caract) classes |ig/=0 && iv/= 0 =  (ig/iv,(Numerico (nome,atributo))):iGR base tamBase caract classes
                                                                |otherwise = (0,(Numerico (nome,atributo))):iGR base tamBase caract classes
                                  where 
-                                ig = iGnumerico base tamBase nome atributo (tail classes) 
-                                iv = iVnumerico base tamBase nome atributo
+                                ig = (iGnumerico base tamBase nome atributo (tail classes)) 
+                                iv = - (iVnumerico base tamBase nome atributo)
     -------------------------------------------------------------------------------------------------------------------------------------------------------
     --------------Calculo de razão de ganho para valores nominais---------------------
     --Função auxiliar para calcular o iG
     --Entrada: nome da caracteristica, os atributos possíveis, uma base de exemplo, o tamanho da base e as classes possíveis
     --Saida: Um valor númerico para o calculo do iG
-    valoresIG :: String->[String]->[Exemplo]->Int->[String]->Double
+    --valoresIG :: String->[String]->[Exemplo]->Int->[String]->Double
     valoresIG _ [] _ _ _ = 0
     valoresIG nome (atb:atributos) base tamBase classes | tamConjunto /=0 && tamBase /=0 = ((tamConjunto/(fromIntegral tamBase))*(entropiaX))+(valoresIG nome atributos base tamBase  classes) 
                                                         | otherwise = valoresIG nome atributos base tamBase classes 
@@ -80,7 +80,7 @@ module ArvoreDecision where
     --Função para o calculo do iG
     --Entrada: Base, tamanho da base, nome da caracteristica, os atributos e as possíveis classes
     --Saida: Um valor númerico que represanta o iG
-    iG :: [Exemplo]->Int->String->[String]->[String]->Double
+    --iG :: [Exemplo]->Int->String->[String]->[String]->Double
     iG base tamBase nome atributo classes = entropiaX -  valoresX
                 where
                 entropiaX = entropia base classes
@@ -89,9 +89,9 @@ module ArvoreDecision where
     --Função para calcular o iV
     --Entrada: Base, tamanho da base, nome da caracteristica e o conjunto de valores
     --Saida: Um valor númerico representando o iV
-    iV::[Exemplo]->Int->String->[String]->Double
+    --iV::[Exemplo]->Int->String->[String]->Double
     iV _ _ _ [] = 0
-    iV base tamBase nome (atb:atributo) | tamConjunto /=0 && tamBase /=0 = -percentagem*logBase(2)(percentagem) + (iV base tamBase nome atributo)
+    iV base tamBase nome (atb:atributo) | tamConjunto /=0 && tamBase /=0 = percentagem*logBase(2)(percentagem) + (iV base tamBase nome atributo)
                                         |otherwise =  (iV base tamBase nome atributo)
                         where 
                             novoConjunto = filtraAtributo nome atb base
@@ -102,7 +102,7 @@ module ArvoreDecision where
     --Função do calculo de iG especifico para caracteristicas numericas
     --Entrada: Base, tamanho da base, nome da caracteristica, o conjunto de valores (intervalos) e as possíveis classes
     --Saida: Um valor númerico representando iG
-    iGnumerico::[Exemplo]->Int->String->[(String,String)]->[String]->Double
+    --iGnumerico::[Exemplo]->Int->String->[(String,String)]->[String]->Double
     iGnumerico base tamBase nome atributo classes = entropiaX -  valoresX
                 where
                 entropiaX = entropia base classes
@@ -110,7 +110,7 @@ module ArvoreDecision where
     --Função auxiliar para o calculo do iG especifico para caracteristicas númericas
     --Entrada: Nome da caracteristica, list de valores possíveis (intervalos), uma base, tamanho da base e as possíveis classes
     --Saida: Valor representativo para o calculo do iG
-    valoresIGnumerico::String->[(String,String)]->[Exemplo]->Int->[String]->Double
+    --valoresIGnumerico::String->[(String,String)]->[Exemplo]->Int->[String]->Double
     valoresIGnumerico _ [] _ _ _ = 0
     valoresIGnumerico nome (atb:atributos) base tamBase classes  |tamConjunto /=0 && tamBase /=0 = ((tamConjunto/(fromIntegral tamBase))*(entropiaX))+(valoresIGnumerico nome atributos base tamBase  classes) 
                                                                  | otherwise = valoresIGnumerico nome atributos base tamBase classes 
@@ -121,9 +121,9 @@ module ArvoreDecision where
     --Função especifica para o calculo do iV de caracteristicas numericas
     --Entrada:Base, tamanho da base, nome da caracteristica e uma lista de intervalos numericos, que são os valores
     --Saida: Valor númerico representativo de iV
-    iVnumerico::[Exemplo]->Int->String->[(String,String)]->Double
+    --iVnumerico::[Exemplo]->Int->String->[(String,String)]->Double
     iVnumerico _ _ _ [] = 0
-    iVnumerico base tamBase nome (atb:atributo) | tamConjunto /=0 && tamBase /=0 = -percentagem*logBase(2)(percentagem) + (iVnumerico base tamBase nome atributo)
+    iVnumerico base tamBase nome (atb:atributo) | tamConjunto /=0 && tamBase /=0 = percentagem*logBase(2)(percentagem) + (iVnumerico base tamBase nome atributo)
                                                 |otherwise =  (iVnumerico base tamBase nome atributo)
                       where 
                           novoConjunto = filtraAtributoNum nome atb base
@@ -133,7 +133,7 @@ module ArvoreDecision where
     ---------------------------------------------Calculo da entropia-----------------------------------------------
     --Entrada: Base e possíveis classes
     --Saida: Valor númerico representativo da entropia
-    entropia:: [Exemplo]->[String]->Double
+    --entropia:: [Exemplo]->[String]->Double
     entropia base [] = 0
     entropia base (ci:classes) |percentagem /=0 = (-percentagem)*(logBase(2)(percentagem)) + entropia base classes
                                |otherwise = entropia base classes
