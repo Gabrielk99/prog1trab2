49537d9f982161feca4cacc83c68133be6f7d0f5
Gabrielk99 <34044829+Gabrielk99@users.noreply.github.com>
2019-07-11 17:05:02 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ import Debug.Trace
 import Utils
 import ArvoreDecision
 import GeraSaida
+import System.IO
 import qualified Data.Map as Map
 main = do 
     descricao<-readFile "descricao.txt"
@@ -11,20 +12,14 @@ main = do
     let base = map (words) (lines exemplos)
     let classe =(last descricao_1)
     let caracteristicas = (convertToCaracteristicas descricao_1)
-    let base_1 = convertToExemplo base descricao_1
-    let maisComum = maioria base_1 (tail(last descricao_1))
+    let base_1 = convertToExemplo base caracteristicas
+    let maisComum = maioria base_1 (tail classe) (head classe)
     let arvD = arvoreDecisao base_1 (init caracteristicas) maisComum classe
     caso<-readFile "caso.txt"
-    let casos = convertToExemplo (map (words) (lines caso)) descricao_1
+    let casos = convertToExemplo (map (words) (lines caso)) caracteristicas
     let result = gerarResposta casos arvD
     writeFile "classe.txt" (formataSaida result) 
-
-
+    --putStrLn $ show(discretiza caracteristicas base_1 (head classe))
+    writeFile "arvore.txt" (geraSaidaTextual arvD "")
+    putStrLn $ show(arvD)
 ---------------------------------------------------------------------------------------------------
-{-
-geraSaidaTextual' (Valor (Num (min,max))filha) | min == ((read "-Infinity")::Double) = "<= "++(show max)++geraSaidaTextual filha
-                                                | max == ((read "Infinity")::Double) = ">"++(show min)++geraSaidaTextual filha
-                                                | otherwise = ">"++ (show min) ++ "<="++(show max)++geraSaidaTextual filha 
-geraSaidaTextual (Caracteristica nome filhas) = "se " ++ nome ++ saida
-                                            where 
-                                                saida = (concat[geraSaidaTextual' valor | valor <- filhas]) -}
